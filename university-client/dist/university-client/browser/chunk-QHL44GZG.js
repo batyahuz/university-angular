847834f825c12418c310f6fd83b4348a5c5f1187
Batya Hutzler <batyahuz@gmail.com>
2024-03-14 22:23:33 +0200

    build the project

---
+++ university-client/dist/university-client/browser/chunk-QHL44GZG.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-AIFR7DEA.js";import"./chunk-BOMFQHQL.js";export{a as UsersModule};
---
+++ university-client/dist/university-client/browser/chunk-YVVKN22Y.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-W46OFFQU.js";import"./chunk-BOMFQHQL.js";export{a as CoursesModule};
